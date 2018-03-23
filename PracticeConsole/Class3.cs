b393574bfce23fc0b60ff0bf91745106db3e9a69
owaisshahab <owaisshahab@gmail.com>
2018-03-23 12:44:54 +0500

    ...

--- PracticeConsole/Class3.cs
+++ PracticeConsole/Class3.cs
@@ -337,5 +337,183 @@ namespace PracticeConsole
             Console.ReadKey();
         }
         */
+
+
+        /* //Use  counting  of  the  brackets:  For  an  opening  bracket  increase  the 
+        // counter by 1 and for closing bracket decrease it by 1. Watch the counter
+        // not to become a negative number and always ends with 0.
+
+         static void Main(string[] args)
+         {
+             int count = 0;
+             string str = "((((()))))";
+             string[] strArray = new string[100];
+             strArray = str.Split('(');
+             foreach (var item in strArray)
+             {
+                 if (item == "")
+                 {
+                     ++count;
+                 }
+             }
+
+             strArray = str.Split(')');
+
+             foreach (var item in strArray)
+             {
+                 if (item == "")
+                 {
+                     --count;
+                 }
+             }
+
+             Console.WriteLine(count);
+             Console.ReadKey();
+         }
+         */
+
+        /*   //Write  a  program  that  reads  a  string,  reverse  it  and  prints  it  to  the 
+       // console.For example: "introduction"  "noitcudortni".
+
+        static void Main(string[] args)
+        {
+            string str = "introduction";
+
+            char[] charArray = new char[100];
+
+            charArray = str.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            foreach (var item in charArray)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadKey();
+        }
+        */
+
+        /*   //  Write  a  program  that  checks  whether  the  parentheses  are  placed 
+        /// correctly  in  an arithmetic  expression.Example of  expression with
+        //correctly placed  brackets:  ((a+b)/5-d).  Example of  an incorrect
+        //expression: )(a+b)).
+
+        static void Main(string[] args)
+        {
+            string str = "((a + b) / 5 - d)";
+            //"((a + b) / 5 - d)";
+            //string incorrect = ")(a+b))";
+
+            int openBracketCounter = 0;
+
+            char[] charArray = new char[100];
+
+            charArray = str.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] == '(')
+                {
+                    ++openBracketCounter;
+                }
+                else if (charArray[i] == ')')
+                {
+                    --openBracketCounter;
+                }
+            }
+
+            Console.WriteLine(openBracketCounter == 0 ? "all brackets are properly closed" : "any brackets are not properly closed");
+            Console.ReadKey();
+
+        }
+        */
+
+        /*   //  Write a program that detects how many times a substring is contained in 
+        //  the text.For example, let’s look for the substring "in" in the text: 
+        static void Main(string[] args)
+        {
+            string str = "We are living in a yellow submarine.We don't have anything else. Inside the submarine is very tight.So we are drinking all the day.We will move out of it in 5 days.";
+            int count = 0;
+            char[] charArray = new char[1000];
+
+            charArray = str.ToCharArray();
+
+            for (int i = 0; i < charArray.Length-1; i++)
+            {
+                //if ( (charArray[i] == ('i') || charArray[i] == ('I')) && (charArray[i+1] == ('n') || charArray[i+1] == ('N')))
+                if ((charArray[i] == ('w') || charArray[i] == ('W')) && (charArray[i + 1] == ('e') || charArray[i + 1] == ('E')))
+                {
+                    ++count;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.ReadKey();
+
+        }
+        */
+
+        /*   //        A text is given. Write a program that  modifies the casing of letters to 
+        //        uppercase at  all places  in  the text  surrounded by<upcase>  and
+        //        </upcase> tags. Tags cannot be nested. 
+        //   Example: 
+        //   We are living in a<upcase> yellow submarine</upcase>.We
+        //   don't have <upcase>anything</upcase> else. 
+        //   Result: 
+        //   We are living in a YELLOW SUBMARINE.We don't have ANYTHING else. 
+
+        static void Main(string[] args)
+        {
+            int length = 2000;
+            string str = "We are living in a<upcase> yellow submarine</upcase>. We don't have <upcase>anything</upcase> else.";
+            char[] chrArray = new char[2000];
+            string[] strArray = new string[str.Length];
+            chrArray = str.ToCharArray();
+            for (int i = 0; i < chrArray.Length; i++)
+            {
+                strArray[i] = chrArray[i].ToString();
+            }
+            int lastIndex = str.IndexOf('>');
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if(strArray[i] == ("<") && strArray[i+1] == ("u") && strArray[i+2] == ("p") && strArray[i+3] == ("c") && strArray[i+4] == ("a") && strArray[i+5] == ("s") && strArray[i+6] == ("e") && strArray[i+7] == (">") )
+                {
+                    strArray[i] = strArray[i].Replace("<", "");
+                    strArray[i + 1] = strArray[i + 1].Replace("u", "");
+                    strArray[i + 2] = strArray[i + 2].Replace("p", "");
+                    strArray[i + 3] = strArray[i + 3].Replace("c", "");
+                    strArray[i + 4] = strArray[i + 4].Replace("a", "");
+                    strArray[i + 5] = strArray[i + 5].Replace("s", "");
+                    strArray[i + 6] = strArray[i + 6].Replace("e", "");
+                    strArray[i + 7] = strArray[i + 7].Replace(">", "");
+
+                    for (int j = i+8; j < strArray.Length-5; j++)
+                    {
+                        strArray[j] = strArray[j].ToString().ToUpper();
+                        if (strArray[j] == ("<") && strArray[j + 1] == ("/") )
+                        {
+                            strArray[j] = strArray[j].Replace("<", "");
+                            strArray[j + 1] = strArray[j+1].Replace("/", "");
+                            strArray[j + 2] = strArray[j+2].Replace("u", "");
+                            strArray[j + 3] = strArray[j+3].Replace("p", "");
+                            strArray[j + 4] = strArray[j+4].Replace("c", "");
+                            strArray[j + 5] =  strArray[j+5].Replace("a", "");
+                            strArray[j + 6] = strArray[j+6].Replace("s", "");
+                            strArray[j + 7] = strArray[j+7].Replace("e", "");
+                            strArray[j + 8] = strArray[j + 7].Replace(">", "");
+                            break;
+                        }
+                    }
+                } 
+            }
+
+            foreach (var item in strArray)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+        */
     }
 }
