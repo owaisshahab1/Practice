6dfb76d80b8246d97e133ac868b6f8f468c8fc73
owais shahab <owais.shahab@innovativesolutions.ae>
2018-05-10 18:08:43 +0500

    add some files but error still there

--- MvcPractice/MvcPractice/App_Start/RouteConfig.cs
+++ MvcPractice/MvcPractice/App_Start/RouteConfig.cs
@@ -9,14 +9,14 @@ namespace MvcPractice
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
+        public static void RegisterRoutes (RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute ("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute (
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Employees", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
