72e591ee827026bcd61b65daf8a78c0b734b3725
owaisshahab <owaisshahab@gmail.com>
2018-05-11 01:35:08 +0500

    100.P(11) C# Database Tutorial In Urdu - Loading Data in &quot;DataGridView&quot; control - YouTube

--- WindowsFormsApplication/ManageStudentForm.Designer.cs
+++ WindowsFormsApplication/ManageStudentForm.Designer.cs
@@ -28,36 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.studentListDataGridView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.studentListDataGridView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // studentListDataGridView
             // 
-            this.button1.Location = new System.Drawing.Point(36, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add New Student";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.studentListDataGridView.AllowUserToAddRows = false;
+            this.studentListDataGridView.AllowUserToDeleteRows = false;
+            this.studentListDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentListDataGridView.Location = new System.Drawing.Point(12, 92);
+            this.studentListDataGridView.MultiSelect = false;
+            this.studentListDataGridView.Name = "studentListDataGridView";
+            this.studentListDataGridView.ReadOnly = true;
+            this.studentListDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.studentListDataGridView.Size = new System.Drawing.Size(659, 203);
+            this.studentListDataGridView.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem,
+            this.newStudentToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.closeToolStripMenuItem.Text = "close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // newStudentToolStripMenuItem
+            // 
+            this.newStudentToolStripMenuItem.Name = "newStudentToolStripMenuItem";
+            this.newStudentToolStripMenuItem.Size = new System.Drawing.Size(118, 20);
+            this.newStudentToolStripMenuItem.Text = "Add New Student";
+            this.newStudentToolStripMenuItem.Click += new System.EventHandler(this.newStudentToolStripMenuItem_Click);
             // 
             // ManageStudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(683, 307);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.studentListDataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ManageStudentForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Manage Student Form";
+            this.Load += new System.EventHandler(this.ManageStudentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentListDataGridView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView studentListDataGridView;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newStudentToolStripMenuItem;
     }
 }
--- WindowsFormsApplication/ManageStudentForm.cs
+++ WindowsFormsApplication/ManageStudentForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,10 +19,38 @@ namespace WindowsFormsApplication
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void newStudentToolStripMenuItem_Click(object sender, EventArgs e)
         {
             StudentInfoForm sif = new StudentInfoForm();
             sif.ShowDialog();
+        }
+
+        private void ManageStudentForm_Load(object sender, EventArgs e)
+        {
+            studentListDataGridView.DataSource = GetStudentList();
+        }
+
+        private DataTable GetStudentList()
+        {
+            DataTable dtStudentData = new DataTable();
+            string connString = ConfigurationManager.ConnectionStrings["dbx"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand("usp_StudentGetAllStudents", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    dtStudentData.Load(reader);
+                }
+            }
+            return dtStudentData;
         }
     }
 }
