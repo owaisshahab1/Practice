dfbe567cee5afc6e84f1de535ec4cab39d93e541
owaisshahab <owaisshahab@gmail.com>
2018-05-12 20:51:21 +0500

    ASP.NET MVC 4 Tutorial In Urdu - Creating Area in ASP.NET MVC Application - YouTube

---
+++ MvcPractice/MvcPractice/Areas/Hr/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Areas.Hr.Models
+{
+    public class DefaultController : Controller
+    {
+        // GET: Hr/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
