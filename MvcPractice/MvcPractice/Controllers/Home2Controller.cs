cc07638500463613156a453df119b902b0f97ec5
owaisshahab <owaisshahab@gmail.com>
2018-05-12 12:40:25 +0500

    ASP.NET MVC 4 Tutorial In Urdu - Introduction to MVC - YouTube

---
+++ MvcPractice/MvcPractice/Controllers/Home2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string WelcomeMessage()
+        {
+            return "Welcome to Asp.Net MVC";
+        }
+    }
+}
