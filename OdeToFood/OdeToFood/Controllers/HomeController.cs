8fece0b5e57099637d17314b9a77e78d36214ef4
owaisshahab <owaisshahab@gmail.com>
2018-05-15 22:42:35 +0500

    2-Routes and Controllers

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ namespace OdeToFood.Controllers
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+            var message = string.Format("{0}::{1}{2}", controller, action, id);
+            ViewBag.Message = message;
             return View();
         }
 
