0b5c72e2539cdc5b576c2d00f26a756748dd4968
owais shahab <owais.shahab@innovativesolutions.ae>
2018-05-16 16:39:35 +0500

    old mvc project added again

---
+++ MvcPractice/MvcPractice/Areas/Hr/HrAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MvcPractice.Areas.Hr
+{
+    public class HrAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Hr";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Hr_default",
+                "Hr/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "MvcPractice.Areas.Hr.Models" }
+            );
+        }
+    }
+}
---
+++ MvcPractice/MvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string WelcomeMessage()
+        {
+            return "Welcome to Asp.Net MVC";
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Insert ()
+        {
+            return View ();
+        }
+    }
+}
