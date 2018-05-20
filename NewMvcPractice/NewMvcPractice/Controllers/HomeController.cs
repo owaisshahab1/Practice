28b3a9cbfe28912f0efb4309891e9d6b426d94cc
owaisshahab <owaisshahab@gmail.com>
2018-05-20 16:14:54 +0500

    07.ASP.NET MVC 4 Database Tutorial In Urdu - Entity Framework (CRUD Operations)

--- NewMvcPractice/NewMvcPractice/Controllers/HomeController.cs
+++ NewMvcPractice/NewMvcPractice/Controllers/HomeController.cs
@@ -17,5 +17,58 @@ namespace NewMvcPractice.Controllers
             List<Employee> employees = employeeVM.GetAllEmployee();
             return View(employees);
         }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeViewModel employeeVm = new EmployeeViewModel();
+                employeeVm.AddNewEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            EmployeeViewModel employeeVm = new EmployeeViewModel();
+            Employee employee = employeeVm.GetEmployeeDetailsById(id);
+            return View(employee);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            EmployeeViewModel employeeVm = new EmployeeViewModel();
+            Employee employee = employeeVm.GetEmployeeDetailsById(id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeViewModel employeeVm = new EmployeeViewModel();
+                employeeVm.UpdateEmployeeDetails(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            EmployeeViewModel employeeVm = new EmployeeViewModel();
+            employeeVm.DeleteEmployee(id);
+            return RedirectToAction("Index");
+
+        }
     }
 }
--- NewMvcPractice/NewMvcPractice/ViewModel/Home/EmployeeViewModel.cs
+++ NewMvcPractice/NewMvcPractice/ViewModel/Home/EmployeeViewModel.cs
@@ -13,5 +13,34 @@ namespace NewMvcPractice.ViewModel.Home
             NewMvcPracticeDBContext db = new NewMvcPracticeDBContext();
             return db.Employees.ToList();
         }
+
+        public void AddNewEmployee(Employee employee)
+        {
+            NewMvcPracticeDBContext db = new NewMvcPracticeDBContext();
+            db.Employees.Add(employee);
+            db.SaveChanges();
+        }
+
+        public Employee GetEmployeeDetailsById(int id)
+        {
+            NewMvcPracticeDBContext db = new NewMvcPracticeDBContext();
+            return db.Employees.Find(id);
+        }
+
+        public void UpdateEmployeeDetails(Employee employee)
+        {
+            NewMvcPracticeDBContext db = new NewMvcPracticeDBContext();
+            db.Entry(employee).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            NewMvcPracticeDBContext db = new NewMvcPracticeDBContext();
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+        }
     }
 }
