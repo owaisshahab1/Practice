f9c7ec133694988ef4562a964da0984e421f446d
owaisshahab <owaisshahab@gmail.com>
2018-05-27 19:13:03 +0500

    Part 1 - C# Database Programming In Urdu - UI Design

--- WindowsFormsApplication/Program.cs
+++ WindowsFormsApplication/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication
         {
             Application.EnableVisualStyles ();
             Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run(new EmployeeForm ());
+            Application.Run(new DVLoginPro ());
         }
     }
 }
