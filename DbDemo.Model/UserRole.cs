d66795e72a7049612d8f29c37d191965b138526a
owaisshahab <owaisshahab@gmail.com>
2018-06-02 01:46:10 +0500

    Part 8 - C# Database In Urdu - Splitting the code into layers

---
+++ DbDemo.Model/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbDemo.Model
+{
+    public class UserRole
+    {
+        public int RoleId { get; set; }
+        public string RoleDescription { get; set; }
+        public string RoleNotes { get; set; }
+    }
+}
--- WindowsFormsApplication/DVLoginPro.cs
+++ WindowsFormsApplication/DVLoginPro.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DbDemo.Model;
+using DbDemo.Service;
+using DbDemo.Utilities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -31,42 +34,30 @@ namespace WindowsFormsApplication
 
         private void LoadAllRoles()
         {
-            try
+            List<UserRole> userRoles = new List<UserRole>();
+            LoginService.GetAllRoles(userRoles);
+            foreach (var eachRole in userRoles)
             {
-                string connectionString = ConfigurationManager.ConnectionStrings["loginprobdb"].ConnectionString;
-                SqlConnection conn = new SqlConnection(connectionString);
-                SqlCommand cmd = new SqlCommand("usp_GetAllRoles", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    RoleComboBox.Items.Add(reader[1]);
-                }
-                reader.Close();
-                conn.Close();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.Message);
+                RoleComboBox.Items.Add(eachRole.RoleDescription);
             }
         }
 
         private void SignInButton_Click(object sender, EventArgs e)
         {
-            bool isCredentialsCorrect = false;
-            bool isUserNameCorrect = false;
-            bool isPasswordCorrect = false;
-            bool isRoleCorrect = false;
-            Authenticated(UserNameTextBox.Text,
-                              PasswordTextBox.Text,
-                              RoleComboBox.Text,
-                              ref isCredentialsCorrect,
-                              ref isUserNameCorrect,
-                              ref isPasswordCorrect,
-                              ref isRoleCorrect);
-            if(isCredentialsCorrect)
+            LoginUser loginUser = new LoginUser()
+            {
+                UserName = UserNameTextBox.Text,
+                Password = PasswordTextBox.Text,
+                Role = RoleComboBox.Text,
+                AreCredentialsCorrect = false,
+                IsUserNameCorrect = false,
+                IsPasswordCorrect = false,
+                IsRoleCorrect = false
+            };
+
+            LoginService.Authenticated(loginUser);
+
+            if(loginUser.AreCredentialsCorrect)
             {
                 this.Hide();
                 AdminForm adminForm = new AdminForm();
@@ -74,65 +65,25 @@ namespace WindowsFormsApplication
             }
             else
             {
-                if (!isUserNameCorrect)
+                if (!loginUser.IsUserNameCorrect)
                 {
                     MessageBox.Show("No Such User Exists", "No User Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
-                    if (!isPasswordCorrect && isRoleCorrect)
+                    if (!loginUser.IsPasswordCorrect && loginUser.IsRoleCorrect)
                     {
                         MessageBox.Show("Password is wrong", "Wrong Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-                    if (!isPasswordCorrect && !isRoleCorrect)
+                    if (!loginUser.IsPasswordCorrect && !loginUser.IsRoleCorrect)
                     {
                         MessageBox.Show("Both Password and Role are wrong", "Wrong Password and Role", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-                    if (!isRoleCorrect && isPasswordCorrect)
+                    if (!loginUser.IsRoleCorrect && loginUser.IsPasswordCorrect)
                     {
                         MessageBox.Show("Role is not associated with this user", "Wrong Role", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
-            }
-        }
-
-        private void Authenticated(string _userName,
-                                    string _password,
-                                    string _role,
-                                    ref bool _IsCredentialsCorrect,
-                                    ref bool _IsUserNameCorrect,
-                                    ref bool _IsPasswordCorrect,
-                                    ref bool _IsRoleCorrect)
-        {
-            try
-            {
-                string connectionString = ConfigurationManager.ConnectionStrings["loginprobdb"].ConnectionString;
-                SqlConnection conn = new SqlConnection(connectionString);
-                SqlCommand cmd = new SqlCommand("usp_CheckIsUserCredentialsCorrect3", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@UserName", SqlDbType.NVarChar, 50).Value = _userName;
-                cmd.Parameters.Add("@Password", SqlDbType.NVarChar, 50).Value = _password;
-                cmd.Parameters.Add("@Role", SqlDbType.NVarChar, 50).Value = _role;
-
-                cmd.Parameters.Add("@IsCredentialsCorrect", SqlDbType.Bit).Direction = ParameterDirection.Output;
-                cmd.Parameters.Add("@IsUserNameCorrect", SqlDbType.Bit).Direction = ParameterDirection.Output;
-                cmd.Parameters.Add("@IsPasswordCorrect", SqlDbType.Bit).Direction = ParameterDirection.Output;
-                cmd.Parameters.Add("@IsRoleCorrect", SqlDbType.Bit).Direction = ParameterDirection.Output;
-
-                conn.Open();
-
-                cmd.ExecuteNonQuery();
-
-                _IsCredentialsCorrect = (bool)cmd.Parameters["@IsCredentialsCorrect"].Value;
-                _IsUserNameCorrect = (bool)cmd.Parameters["@IsUserNameCorrect"].Value;
-                _IsPasswordCorrect = (bool)cmd.Parameters["@IsPasswordCorrect"].Value;
-                _IsRoleCorrect = (bool)cmd.Parameters["@IsRoleCorrect"].Value;
-
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.Message);
             }
         }
     }
