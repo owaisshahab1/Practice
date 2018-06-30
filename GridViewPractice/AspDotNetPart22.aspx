bffd457094e08484e93fed19602cc551dda1f376
owaisshahab <owaisshahab@gmail.com>
2018-07-01 03:05:22 +0500

    AspDotNetPart22

---
+++ GridViewPractice/AspDotNetPart22.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AspDotNetPart22.aspx.cs" Inherits="AspDotNetPart22" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:DropDownList ID="ddlCategories" AutoPostBack="true" DataTextField="CategoryName" DataValueField="CategoryID" Width="200px" runat="server" 
+            onselectedindexchanged="ddlCategories_SelectedIndexChanged"></asp:DropDownList>
+    <br /><br />
+        <asp:DropDownList ID="ddlProducts" AutoPostBack="true" 
+            DataTextField="ProductName" DataValueField="ProductID" Width="200px" 
+            runat="server" onselectedindexchanged="ddlProducts_SelectedIndexChanged"></asp:DropDownList>
+    <br /><br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ GridViewPractice/AspDotNetPart22.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+public partial class AspDotNetPart22 : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ddlCategories.DataSource = GetData("spGetCategories", null);
+                ddlCategories.DataBind();
+
+            ListItem liCategory = new ListItem("Select Category","-1");
+            ddlCategories.Items.Insert(0, liCategory);
+
+            ListItem liProduct = new ListItem("Select Product", "-1");
+            ddlProducts.Items.Insert(0, liProduct);
+
+            ddlProducts.Enabled = false;
+        }
+    }
+
+    private DataSet GetData(string SPName, SqlParameter SPParameter)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["NORTHWNDConnectionString"].ConnectionString;
+
+        SqlConnection con = new SqlConnection(cs);
+
+        SqlDataAdapter da = new SqlDataAdapter(SPName, con);
+
+        da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+        if (SPParameter != null)
+        {
+            da.SelectCommand.Parameters.Add(SPParameter);
+        }
+
+        DataSet ds = new DataSet();
+
+        da.Fill(ds);
+
+        return ds;
+
+    }
+    protected void ddlCategories_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCategories.SelectedIndex == 0)
+        {
+            ddlProducts.SelectedIndex = 0;
+            ddlProducts.Enabled = false;
+        }
+        else
+        {
+            ddlProducts.Enabled = true;
+            SqlParameter parameter = new SqlParameter("@CategoryId", ddlCategories.SelectedValue);
+            DataSet ds = GetData("spGetProductsByCategoryId", parameter);
+
+            ddlProducts.DataSource = ds;
+            ddlProducts.DataBind();
+            
+
+            ListItem liProduct = new ListItem("Select Product", "-1");
+            ddlProducts.Items.Insert(0, liProduct);
+
+            ddlProducts.SelectedIndex = 0;
+            ddlProducts.Enabled = true;
+        }
+    }
+    protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        //==========================TO DO=====================================
+
+        if (ddlProducts.SelectedIndex > 0 && ddlProducts.SelectedValue != null)
+        {
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
