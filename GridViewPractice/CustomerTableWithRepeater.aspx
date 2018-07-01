dae43ab4a2d2535e414b492e62893112a69f23a2
owaisshahab <owaisshahab@gmail.com>
2018-07-01 22:45:34 +0500

    Drowndown with open query from database

---
+++ GridViewPractice/CustomerTableWithRepeater.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CustomerTableWithRepeater.aspx.cs" Inherits="CustomerTableWithRepeater" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="ddlCountry" AutoPostBack="true"  DataTextField="CountryName" 
+            DataValueField="CountryId" runat="server" 
+            onselectedindexchanged="ddlCountry_SelectedIndexChanged" Width="200px">
+        </asp:DropDownList>
+        <asp:DropDownList ID="ddlCity" AutoPostBack="true"  DataTextField="CityName" 
+            DataValueField="CityId" Width="200px" runat="server">
+        </asp:DropDownList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ GridViewPractice/CustomerTableWithRepeater.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class CustomerTableWithRepeater : System.Web.UI.Page
+{
+    string queryString;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string queryString = " Select distinct Country from Customers";
+            ddlCountry.DataSource = GetData(queryString,null);
+            ddlCountry.DataBind();
+        }
+    }
+
+    private DataTable GetData(string queryString, String selectedValue)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["NORTHWNDConnectionString"].ConnectionString;
+
+        SqlConnection con = new SqlConnection(cs);
+
+        //SqlDataAdapter da = new SqlDataAdapter(queryString, con);
+
+        SqlCommand cmd = new SqlCommand(queryString, con);
+
+        cmd.CommandType = CommandType.Text;
+
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+        DataSet ds = new DataSet();
+
+        da.Fill(ds);
+
+        DataTable dt = ds.Tables[0];
+        
+
+        DataTable dt2 = new DataTable() ;
+        dt2.Columns.Add("CountryName", typeof(string));
+        dt2.Columns.Add("CountryId", typeof(int));
+        for (int i = 1; i < dt.Rows.Count+1; i++)
+        {
+            dt2.Rows.Add(dt.Rows[i-1].ItemArray[0].ToString(),i);
+        }
+        Session["CountryTable"] = dt2;
+        return dt2;
+    }
+
+
+
+
+
+    protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCountry.SelectedIndex == 0)
+        {
+            ddlCity.SelectedIndex = 0;
+            ddlCity.Enabled = false;
+        }
+        else
+        {
+            ddlCity.Enabled = true;
+
+            DataTable countryIdToName = (DataTable)Session["CountryTable"];
+
+            var results = from myRow in countryIdToName.AsEnumerable()
+                          where myRow.Field<String>("CountryId") == ddlCountry.SelectedValue
+                          select myRow;
+
+
+            queryString = "Select distinct City from Customers where country =  \'" + ddlCountry.SelectedItem + "\' order by City asc";
+
+            string cs = ConfigurationManager.ConnectionStrings["NORTHWNDConnectionString"].ConnectionString;
+
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            //SqlDataAdapter da = new SqlDataAdapter(queryString, con);
+
+            SqlCommand cmd = new SqlCommand(queryString, con);
+
+            cmd.CommandType = CommandType.Text;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            //DataSet ds = new DataSet();
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            con.Close();
+
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("CityName", typeof(string));
+            dt2.Columns.Add("CityId", typeof(int));
+            for (int i = 1; i < dt.Rows.Count + 1; i++)
+            {
+                dt2.Rows.Add(dt.Rows[i - 1].ItemArray[0].ToString(), i);
+            }
+            Session["CityTable"] = dt2;
+
+            ddlCity.DataSource = dt2;
+            ddlCity.DataBind();
+
+            //SqlParameter parameter = new SqlParameter("@CategoryId", ddlCategories.SelectedValue);
+            //DataSet ds = GetData("spGetProductsByCategoryId", parameter);
+
+            //ddlProducts.DataSource = ds;
+            //ddlProducts.DataBind();
+        }
+    }
+}
